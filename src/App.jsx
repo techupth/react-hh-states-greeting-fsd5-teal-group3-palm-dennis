2ea23fce0d6712b9cebbb0f21c14f43d4444187f
Palm <palml1lax@gmail.com>
2023-12-20 19:04:53 +0700

    feat(App.jsx): finish ex1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [Message, setMessage] = useState("Greeting Message");
+  const th = "สวัสดี!";
+  const engligh = "Hi!";
+  const chinese = "你好!";
+  const x = "1";
+
+  const handleChange = (e) => {
+    if (e.target.innerText === engligh) {
+      setMessage(engligh);
+    } else if (e.target.innerText === chinese) {
+      setMessage(chinese);
+    } else if (e.target.innerText === th) {
+      setMessage(th);
+    } else {
+      setMessage("Greeting Message");
+    }
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{Message}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={handleChange}>{th}</button>
+        <button onClick={handleChange}>{engligh}</button>
+        <button onClick={handleChange}>{chinese}</button>
+        <button onClick={handleChange}>reset</button>
       </div>
     </div>
   );
